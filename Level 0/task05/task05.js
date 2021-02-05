31ae435d973ffc1cdcbe8d6beaa8164836b29175
Rumbi <rumbi@localhost.localdomain>
2021-02-05 10:14:31 +0200

    Task 0.5 commit

---
+++ Level 0/task05/task05.js
@@ -0,0 +1,7 @@
+const triangleArea = (a, b, c) => {
+    let semiParameter = (a + b + c) / 2;
+    
+    return (Math.sqrt(semiParameter * (semiParameter - a) * (semiParameter - b) * (semiParameter - c)));
+}
+
+console.log(triangleArea(6, 6, 6));
