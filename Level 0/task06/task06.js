19ff8437342b33e3727f7973fcd4113d997b9f30
Rumbi <rumbi@localhost.localdomain>
2021-02-07 01:15:40 +0200

    Task 0.6 correction

--- Level 0/task06/task06.js
+++ Level 0/task06/task06.js
@@ -1,18 +1,11 @@
-// const maxNumber = (a, b, c) => {
-//     if (a > b && a > c) return a;
-//     else if (b > a && b > c) return b;
-//     else return c;
-// }
-
 const maximum = (...numbers) => {
-    let temp;
+    let temp = 0;
 
-    for (let v = 1; v < numbers.length; v++) {
+    for (let v = 0; v < numbers.length; v++) {
         if  (typeof numbers[v] !== "number") {
             return "Error: Input numbers only!!"
         }
         
-        temp = numbers[0];
         if (temp < numbers[v]) {
             temp = numbers[v];
         }
