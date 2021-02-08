ee9fd2c4f196fd36bdb5daa5f6cea2255e1a3d86
Rumbi <rumbi@localhost.localdomain>
2021-02-08 13:55:49 +0200

    Fix: Less than 60 round down

--- task08/task08.js
+++ task08/task08.js
@@ -1,7 +1,7 @@
 const timeConverter = (minutes) => {
     let tempMin, tempHr;
-    let hrs = Math.round(minutes / 60);
-    let mins = Math.round(minutes % 60);
+    let hrs = parseInt(minutes / 60);
+    let mins = parseInt(minutes % 60);
 
     if (hrs === 1) tempHr = hrs + " hour, ";
     else tempHr = hrs + " hours, ";
@@ -9,5 +9,6 @@ const timeConverter = (minutes) => {
     if (mins === 1) tempMin = mins + " minute";
     else tempMin = mins + " minutes";
 
+    console.log(tempHr + tempMin)
     return (tempHr + tempMin);
 }
