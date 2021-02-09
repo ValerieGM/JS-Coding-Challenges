b27f867baf1654816854d0212b14fee7f40bc9bf
Rumbi <rumbi@localhost.localdomain>
2021-02-09 18:03:39 +0200

    Fix: Remove duplicates

--- task10/task10.js
+++ task10/task10.js
@@ -1,19 +1,19 @@
 const sameLetter = (string1, string2) => {
     let str = "",
         longest, shortest,
-        temp1 = string1.split(""),
-        temp2 = string2.split("");
+        temp1 = new Set(string1.split("")),
+        temp2 = new Set(string2.split(""))
 
     if (temp1.length > temp2.length) {
-        longest = temp1;
-        shortest = temp2;
+        longest = [...temp1];
+        shortest = [...temp2];
     } else {
-        shortest = temp1;
-        longest = temp2;
+        shortest = [...temp1];
+        longest = [...temp2];
     }
 
-    for (let v = 0; v < shortest.length; v ++) {
-        for (let t = 0; t < longest.length; t++) {
+    for (let v = 0; v < longest.length; v ++) {
+        for (let t = 0; t < shortest.length; t++) {
             if (shortest[t] === longest[v]) {
                 str += shortest[t] + ", ";
             }
